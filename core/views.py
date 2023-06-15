5be8989653d432c68dead6b232c6db2950caad4a
Kevin Loureço <81389126+kevinsplourenco@users.noreply.github.com>
2023-06-14 23:40:41 -0300

    Criado listagem de eventos

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from core.models import Evento
+
 
 # Create your views here.
+
+def lista_eventos(request):
+    # usuario = request.user
+    eventos = Evento.objects.all()
+    context = {'eventos': eventos}
+    return render(request, 'agenda.html', context)
